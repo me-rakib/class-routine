ce8410a277ecd20c33037fcd53b906d589cae6f3
Tanaf <tanaf.dev@gmail.com>
2021-05-11 00:45:26 +0600

    V1.0.2 - change timezone

--- assets/main.js
+++ assets/main.js
@@ -34,9 +34,9 @@ const ongoingBtn = document.getElementById("class-name");
 
 // ongoing class, if condition meet then show class name on ongoing status
 const onGoingClass = (className, classId) => {
-  const classIdStyle = document.getElementById(classId)
-    ongoingBtn.textContent = className;
-    classIdStyle.classList.add('ongoing-animated')
+  const classIdStyle = document.getElementById(classId);
+  ongoingBtn.textContent = className;
+  classIdStyle.classList.add("ongoing-animated");
 };
 
 // check day
@@ -50,74 +50,76 @@ const dayCheck = (day, current) => {
 
 //check time
 const timeCheck = (startH, startM, endH, endM) => {
+  process.env.TZ = "Asia/Delhi";
   const currentTime = new Date();
   const startTime = new Date();
   startTime.setHours(startH, startM, 0);
   const endTime = new Date();
   endTime.setHours(endH, endM, 0);
   if (currentTime >= startTime && currentTime < endTime) {
-    return true
+    return true;
   } else {
-    return false
+    return false;
   }
 };
 
 // if day and time true then select class
 const checkDayAndTime = (currentDay) => {
-    if (dayCheck("Monday", currentDay)) {
-        if(timeCheck(9, 00, 9, 30)) {
-            onGoingClass("PSC", "mon-1")
-        } else if(timeCheck(10,30,11,45)) {
-            onGoingClass("PH111", "mon-2")
-        } else if(timeCheck(14,00,14,30)) {
-            onGoingClass("MA111", "mon-3")
-        }
-    } else if (dayCheck("Tuesday", currentDay)) {
-        if(timeCheck(9,00,9,30)) {
-            onGoingClass("MA111", "tue-1")
-        } else if(timeCheck(00,00,10,15)) {
-            onGoingClass("PSC", "tue-2")
-        } else if (timeCheck(10,30,11,45)) {
-            onGoingClass("CS111", "tue-3")
-        }
-    } else if (dayCheck("Wednesday", currentDay)) {
-        if(timeCheck(8,15,8,45)) {
-            onGoingClass("PH110", "wed-1")
-        } else if(timeCheck(10,30,11,00)) {
-              onGoingClass("PSC", "wed-2")
-        } else if(timeCheck(11,15,11,45)) {
-            onGoingClass("MA111", "wed-3")
-        } else if(timeCheck(14,00,15,15)) {
-            onGoingClass("CY111", "wed-4")
-        }
-    } else if(dayCheck("Thursday", currentDay)) {
-        if(timeCheck(8,15,8,45)) {
-            onGoingClass("ME110", "thu-1")
-        } else if(timeCheck(9,00,9,30)) {
-            onGoingClass("PH110", "thu-2")
-        } else if(timeCheck(10,30,11,45)) {
-            onGoingClass("PSC", "thu-3")
-        } else if(timeCheck(14,00,14,30)) {
-            onGoingClass("ME111", "thu-4")
-        }
-    } else if(dayCheck("Friday", currentDay)) {
-        if(timeCheck(8,15,8,45)) {
-            onGoingClass("ME110", "fri-1")
-        } else if(timeCheck(9,45,10,15)) {
-            onGoingClass("PH110", "fri-2")
-        } else if(timeCheck(10,30,11,45)) {
-            onGoingClass("PSC", "fri-3")
-        } else if(timeCheck(14,00,16,00)) {
-            onGoingClass("ME111", "fri-3")
-        }
+  if (dayCheck("Monday", currentDay)) {
+    if (timeCheck(9, 00, 9, 30)) {
+      onGoingClass("PSC", "mon-1");
+    } else if (timeCheck(10, 30, 11, 45)) {
+      onGoingClass("PH111", "mon-2");
+    } else if (timeCheck(14, 00, 14, 30)) {
+      onGoingClass("MA111", "mon-3");
     }
-}
+  } else if (dayCheck("Tuesday", currentDay)) {
+    if (timeCheck(9, 00, 9, 30)) {
+      onGoingClass("MA111", "tue-1");
+    } else if (timeCheck(9, 45, 10, 15)) {
+      onGoingClass("PSC", "tue-2");
+    } else if (timeCheck(10, 30, 11, 45)) {
+      onGoingClass("CS111", "tue-3");
+    }
+  } else if (dayCheck("Wednesday", currentDay)) {
+    if (timeCheck(8, 15, 8, 45)) {
+      onGoingClass("PH110", "wed-1");
+    } else if (timeCheck(10, 30, 11, 00)) {
+      onGoingClass("PSC", "wed-2");
+    } else if (timeCheck(11, 15, 11, 45)) {
+      onGoingClass("MA111", "wed-3");
+    } else if (timeCheck(14, 00, 15, 15)) {
+      onGoingClass("CY111", "wed-4");
+    }
+  } else if (dayCheck("Thursday", currentDay)) {
+    if (timeCheck(8, 15, 8, 45)) {
+      onGoingClass("ME110", "thu-1");
+    } else if (timeCheck(9, 00, 9, 30)) {
+      onGoingClass("PH110", "thu-2");
+    } else if (timeCheck(10, 30, 11, 45)) {
+      onGoingClass("PSC", "thu-3");
+    } else if (timeCheck(14, 00, 14, 30)) {
+      onGoingClass("ME111", "thu-4");
+    }
+  } else if (dayCheck("Friday", currentDay)) {
+    if (timeCheck(8, 15, 8, 45)) {
+      onGoingClass("ME110", "fri-1");
+    } else if (timeCheck(9, 45, 10, 15)) {
+      onGoingClass("PH110", "fri-2");
+    } else if (timeCheck(10, 30, 11, 45)) {
+      onGoingClass("PSC", "fri-3");
+    } else if (timeCheck(14, 00, 16, 00)) {
+      onGoingClass("ME111", "fri-3");
+    }
+  }
+};
 
 const countTime = setInterval(() => {
+  process.env.TZ = "Asia/Delhi";
   const time = new Date();
   const currentDate = `${time.toString().slice(4, 10)}, ${time.getFullYear()}`;
   const currentDay = time.toLocaleString("default", { weekday: "long" });
-  
+
   // show date
   showDate.textContent = currentDate;
   showDay.textContent = currentDay;
@@ -126,6 +128,3 @@ const countTime = setInterval(() => {
   //which class is ongoin now
   checkDayAndTime(currentDay);
 }, 1000);
-
-
-
